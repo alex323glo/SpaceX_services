c1b3bd201c8d93a6d54d8e876f91114127c37f6f
alex323glo <alex323glo@gmail.com>
2017-10-30 22:53:27 +0200

    Added Travis integration. Added global resources.

---
+++ src/test/java/TestTravis.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by alex323glo on 29.10.17.
+ */
+public class TestTravis {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
