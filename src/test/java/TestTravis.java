e59dca8341f0e5019406ff4b6058f8db4152e344
alex323glo <alex323glo@gmail.com>
2017-10-31 00:07:26 +0200

    Test travis.

---
+++ src/test/java/TestTravis.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by alex323glo on 31.10.17.
+ */
+public class TestTravis {
+
+    @Test
+    public void simplePassedTest() {
+        Assert.assertTrue(true);
+    }
+
+}
