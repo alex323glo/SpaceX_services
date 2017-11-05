0ab4d446df830c82b5a6154b8cf9cf1d7706fe93
alex323glo <alex323glo@gmail.com>
2017-11-05 22:07:55 +0200

    Fixed main class (Run). Updated build.gradle file (added dependencies: Mockito).

--- src/main/java/com/alex323glo/spacex/Run.java
+++ src/main/java/com/alex323glo/spacex/Run.java
@@ -1,7 +1,11 @@
 package com.alex323glo.spacex;
 
+import com.alex323glo.spacex.config.ConfigHolder;
+import com.alex323glo.spacex.config.ObjectHolder;
 import com.alex323glo.spacex.rest.JettyServer;
 import com.alex323glo.spacex.rest.SpaceXServer;
+
+import java.io.IOException;
 
 // TODO add doc
 /**
@@ -9,10 +13,18 @@ import com.alex323glo.spacex.rest.SpaceXServer;
  */
 public class Run {
 
-
+    private static ObjectHolder objectHolder;
+    private static ConfigHolder configHolder;
 
     public static void main(String[] args) {
-        SpaceXServer spaceXServer = new JettyServer(8000);
+
+        if (!prepareBeforeRun()) {
+            System.out.println("Can't prepare before main() run!");
+            return;
+        }
+
+        SpaceXServer spaceXServer = new JettyServer(
+                Integer.valueOf(configHolder.getProperty("app.port")));
 
         try {
             spaceXServer.start();
@@ -20,6 +32,23 @@ public class Run {
             System.out.println("Can't start server!!!");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Run all initialisations before running main().
+     *
+     * @see ObjectHolder
+     * @see ConfigHolder
+     * */
+    private static boolean prepareBeforeRun() {
+        objectHolder = ObjectHolder.getInstance();
+
+        configHolder = ConfigHolder.getInstance();
+        if (configHolder == null) {
+            return false;
+        }
+
+        return true;
     }
 
 }
