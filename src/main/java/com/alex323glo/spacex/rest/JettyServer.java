27b75162a587d1e75cae2fafbc52199e41366cd5
alex323glo <alex323glo@gmail.com>
2017-11-07 00:44:56 +0200

    Refactored JettyServer. Added doc to SpaceXServer and JettyServer.

--- src/main/java/com/alex323glo/spacex/rest/JettyServer.java
+++ src/main/java/com/alex323glo/spacex/rest/JettyServer.java
@@ -1,46 +1,81 @@
 package com.alex323glo.spacex.rest;
 
+import com.alex323glo.spacex.exception.ArgumentValidationException;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletHandler;
 
-// TODO create doc
 /**
- * Created by alex323glo on 27.10.17.
- */
+ * Implementation of Embedded HTTP Server powered
+ * by Jetty Server Tools.
+ *
+ * @author alex323glo
+ * @version 1.0.0
+ *
+ * @see Server
+ * @see Connector
+ * @see ServerConnector
+ * @see ServletHandler
+ * */
 public class JettyServer implements SpaceXServer {
-
-    private static final int DEFAULT_PORT = 8000;
 
     private Server server;
 
-    public JettyServer(int port) {
+    /**
+     * Constructor, which creates Jetty Server with needed port and host name.
+     *
+     * @param host host name, which will be set to server.
+     * @param port port number, which will be set to server.
+     * @throws ArgumentValidationException if host or port are not valid.
+     *
+     * @see ArgumentValidationException
+     * */
+    public JettyServer(String host, int port) throws ArgumentValidationException {
+        // Validation:
+        if (host == null) {
+            throw new ArgumentValidationException("host is null");
+        }
+        if (port < 1) {
+            throw new ArgumentValidationException("port number is less then 1");
+        }
+
         // Create server and server's connector:
         server = new Server();
-        ServerConnector connector = new ServerConnector(server);
 
-        // Setup server:
-        connector.setPort(port > 0 ? port : DEFAULT_PORT);
-        server.setConnectors(new Connector[]{connector});
+        // Setup server using ServerConnector:
+        ServerConnector serverConnector = new ServerConnector(server);
+        serverConnector.setPort(port);
+        serverConnector.setHost(host);
+        server.setConnectors(new Connector[]{serverConnector});
 
-        // Add servlet handler to server:
+        // Add Servlet handler to server:
         ServletHandler servletHandler = new ServletHandler();
         server.setHandler(servletHandler);
 
-        // Add servlets to servlet handler:
-        servletHandler.addServletWithMapping(TestServlet.class,"/test");    // test servlet
+        // Add Servlets to Servlet handler:
+        servletHandler.addServletWithMapping(TestServlet.class,"/test");
         servletHandler.addServletWithMapping(LoadPublicFilesServlet.class, "/public/*");
         servletHandler.addServletWithMapping(LoadAjaxServlet.class, "/load-ajax/*");
-        // Other servlets...
     }
 
-
+    /**
+     * Starts HTTP server.
+     *
+     * @throws Exception when has some problems with HTTP Server tools.
+     * @see Exception
+     */
     @Override
     public void start() throws Exception {
         server.start();
     }
 
+    /**
+     * Stops HTTP server.
+     *
+     * @throws Exception when has some problems with HTTP Server tools.
+     * @see Exception
+     */
     @Override
     public void stop() throws Exception {
         server.stop();
--- src/main/java/com/alex323glo/spacex/rest/SpaceXServer.java
+++ src/main/java/com/alex323glo/spacex/rest/SpaceXServer.java
@@ -1,10 +1,27 @@
 package com.alex323glo.spacex.rest;
 
-// TODO create doc
+/**
+ * General interface for embedded HTTP Server.
+ *
+ * @author alex323glo
+ * @version 1.0.0
+ * */
 public interface SpaceXServer {
 
+    /**
+     * Starts HTTP server.
+     *
+     * @throws Exception when has some problems with HTTP Server tools.
+     * @see Exception
+     * */
     void start() throws Exception;
 
+    /**
+     * Stops HTTP server.
+     *
+     * @throws Exception when has some problems with HTTP Server tools.
+     * @see Exception
+     * */
     void stop() throws Exception;
 
 }
